08aa790de99570d7d7345c17963c19007cd11cc0
shawn <13626862366@163.com>
2023-03-05 23:46:58 +0800

    Site updated: 2023-03-05 23:46:54

--- photos/ins.js
+++ photos/ins.js
@@ -116,9 +116,6 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          // var minSrc = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/min_photos/' + data.link[i];
-          // var src = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/photos/' + data.link[i];
- 
           var minSrc = 'https://picblog.shawncoding.top/blogpic/min_photos/' + data.link[i];
           var src = 'https://picblog.shawncoding.top/blogpic/photos/' + data.link[i];
           var type = data.type[i];
@@ -127,7 +124,7 @@
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/photos/ins/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
